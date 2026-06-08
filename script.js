50d8a5375728f3b577bd54957e8af535703f55ff
Atharva Pawar <164200355+Atharvapawar007@users.noreply.github.com>
2026-06-08 04:45:09 +0000

    successfully implemented and tested getComputerChoice function

--- script.js
+++ script.js
@@ -1,1 +1,14 @@
 console.log("Hello World");
+
+function getComputerChoice(){
+    const randomNumber = Math.floor(Math.random() * 100);
+
+    if(randomNumber > 66){
+        return "rock";
+    }else if(randomNumber > 33){
+        return "paper";
+    }else{
+        return "scissors";
+    }
+}
+
